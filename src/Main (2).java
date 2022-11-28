42673067c53bdb82f6ddc0711b8cc70722154945
Manasa-Mamillapalli <smanasa755@gmail.com>
2022-11-28 11:39:22 -0600

    caling methods, instantiations setter and getter methods
    caling methods, instantiations
    setter and getter methods

---
+++ src/Main (2).java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee();
+        employee1.setId(1);
+        employee1.setFirstName("M");
+        employee1.setLastName("sai");
+    }
+}
