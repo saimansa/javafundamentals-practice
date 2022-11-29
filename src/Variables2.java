65e7e08e769b0b64c7a34771240fe49a2fd7c995
Manasa-Mamillapalli <smanasa755@gmail.com>
2022-11-28 23:32:51 -0600

    wrapper classes
    wrapper classes

---
+++ src/Variables2.java
@@ -0,0 +1,8 @@
+public class Variables2 {
+    int i = 1;
+    double d = 1.2;
+
+    //this is class
+    Integer i1 = 1;
+    Double d1 = 1.2;  // this is happening here Double d2 = new Double(1.2)
+}
