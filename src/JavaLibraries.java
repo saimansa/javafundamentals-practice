a04a9bff2e143c71ee8fc1a6c7d751f7761b9a6d
Manasa-Mamillapalli <smanasa755@gmail.com>
2022-12-07 18:56:52 -0600

    Use of Date and time

--- src/JavaLibraries.java
+++ src/JavaLibraries.java
@@ -1,4 +1,9 @@
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -22,5 +27,22 @@ public class JavaLibraries {
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String prettyDate = sdf.format(birthDate);
+        System.out.println(prettyDate);
+
+        LocalDateTime theDateAndTime = LocalDateTime.now();
+        LocalDateTime anotherDateAndTime = LocalDateTime.of(2022, 12, 06, 10, 30 );
+        LocalDate someDate = LocalDate.of(2022,12, 02);
+        LocalTime  someTime = LocalTime.from(anotherDateAndTime);
+
+        System.out.println(theDateAndTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm")));
+        System.out.println(someTime);
+
+        LocalDate yesterday = LocalDate.now();
+        yesterday.minusDays(1);
+
+        LocalDate myBirthday = LocalDate.of(1995, 07, 02);
+        Period difference = Period.between(myBirthday, yesterday);
+
+        System.out.println("There are" + difference.getYears() + "years and" + difference.getMonths() + difference.getDays() + "days between two dates");
     }
 }
